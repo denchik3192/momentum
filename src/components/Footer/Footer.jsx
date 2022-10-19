81242be5994f3cafe137ed75b1495a7aa28f6dfd
DENIS <den465341@gmail.com>
2022-10-19 22:23:01 +0300

    feat: add todo toggle behavior

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,16 @@ import s from "./footer.module.scss";
 import ToDo from "./todo/ToDo";
 
 const Footer = (props) => {
-  const [todoActive, setTodoActive] = useState(true);
+  const [todoActive, setTodoActive] = useState(false);
+
+  const toggleToDo = () => {
+    todoActive ? setTodoActive(false) : setTodoActive(true)
+  }
+
   return <footer className={s.footer}>
     <div className="settings">settings</div>
     <div className="qoute">Spread love everywhere you go. Let no one ever come to you without leaving happier.</div>
-    <div className={s.todo} onClick={()=> setTodoActive(true)}>ToDo</div>
+    <div className={s.todo} onClick={toggleToDo}>ToDo</div>
     <ToDo active={todoActive} setActive={setTodoActive} />
   </footer>;
 };
--- src/components/Footer/todo/ToDo.jsx
+++ src/components/Footer/todo/ToDo.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import s from "./todo.module.scss";
+import cn from "classnames";
 
-const ToDo = ({active, setActive}) => {
-  return <div className={s.todo}>
-    <div className={s.todoHedaer}>Today</div>
-    <div className="qoute">Spread love everywhere you go. Let no one ever come to you without leaving happier.</div>
-    <div className={s.todoFooter}>ToDo</div>
-  </div>;
+const ToDo = ({ active, setActive }) => {
+  return (
+    <div className={active ? cn(s.todo, s.active) : s.todo}>
+      <div className={s.todoHedaer}>Today</div>
+      <div className={s.todoItem}>
+        <input type="checkbox" name="" id="" />
+      </div>
+      <div className={s.todoItem}>
+        <input type="checkbox" name="" id="" />
+      </div>
+      <div className={s.todoItem}>
+        <input type="checkbox" name="" id="" />
+      </div>
+
+      <div className={s.todoFooter}>ToDo</div>
+    </div>
+  );
 };
 
 export default ToDo;
