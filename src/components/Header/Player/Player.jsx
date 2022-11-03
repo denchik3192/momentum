1d2ff946c67b8de523c895f98cf1b58e6bf52720
DENIS <den465341@gmail.com>
2022-11-03 20:59:26 +0300

    feat: add play-list

--- src/components/Header/Player/Player.jsx
+++ src/components/Header/Player/Player.jsx
@@ -1,12 +1,27 @@
+import classNames from "classnames";
 import React from "react";
 import s from "./player.module.scss";
+import playList from "./playList";
+import { CiVolumeHigh } from 'react-icons/';
 
 const Player = (props) => {
+
+  const PlayListItem = ({songName}) => {
+    return (
+      <li className={s.playListItem}><CiVolumeHigh/> {songName}</li>
+    )
+  }
+
+  const songs = playList.map((s) => <PlayListItem songName={s.title} key={s.id}/> )
+
   return (
+    <div>
     <div className={s.player}>
       <button className={s.playPrev}></button>
       <button className={s.play}></button>
       <button className={s.playNext}></button>
+    </div>
+    <ul className={s.playList}>{songs}</ul>
     </div>
   );
 };
