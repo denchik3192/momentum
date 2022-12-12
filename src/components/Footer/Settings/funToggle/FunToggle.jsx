da65989b0e748cb2aac70190531e95dc451c4c57
DENIS <den465341@gmail.com>
2022-12-12 21:36:18 +0300

    feat: add theme context

--- src/components/Footer/Settings/funToggle/FunToggle.jsx
+++ src/components/Footer/Settings/funToggle/FunToggle.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import s from "./funToggle.module.scss";
 
-const FunToggle = () => {
-  
-  return <div>
-          <input id="toggle" className={s.toggle} type="checkbox" role="switch" name="toggle" value="on"/>
-            <label for="toggle" className={s.slot}>
-              <span className={s.slot__label}>OFF</span>
-              <span className={s.slot__label}>ON</span>
-            </label>
-          <div className={s.curtain}></div>
-          </div>
+const FunToggle = ({ value, onChange, name}) => {
+  return (
+    <div>
+      <input
+        id="toggle"
+        className={s.toggle}
+        type="checkbox"
+        onChange={onChange}
+        role="switch"
+        name="toggle"
+        value="on"
+      />
+      <label for="toggle" className={s.slot}>
+        <span className={s.slot__label}>OFF</span>
+        <span className={s.slot__label}>ON</span>
+      </label>
+      <div className={s.curtain}></div>
+    </div>
+  );
 };
 
 export default FunToggle;
