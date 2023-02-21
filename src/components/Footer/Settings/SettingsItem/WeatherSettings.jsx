ed3a8d9834c66ddd339aca92416a199b7bdc7eb1
DENIS <den465341@gmail.com>
2023-02-21 21:58:51 +0300

    feat: add wether settings

--- src/components/Footer/Settings/SettingsItem/WeatherSettings.jsx
+++ src/components/Footer/Settings/SettingsItem/WeatherSettings.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+
+import React from "react";
+import { useSelector } from "react-redux";
+import s from './weatherSettingItem.module.scss'
 
 function WeatherSettings() {
+  const weatherData = useSelector((state) => state.weather);
+
+  const weatherSettingsList = Object.keys(weatherData).slice(0, -2).map((el) => {
+    return (
+      <div div className={s.weatherMainSettings}>
+        <input className={s.item} type="checkbox" name={el} id={el} checked={true}/>
+        <label className={s.labelItem} htmlFor={el}>{el}</label>
+      </div>
+    );
+  });
+
   return (
-    <div>WeatherSettings</div>
-  )
+    <>
+        <h3>Weather</h3>
+        {weatherSettingsList}
+        <button className={s.applyButton}type="submit">Apply</button>
+    </>
+  );
 }
 
-export default WeatherSettings
+export default WeatherSettings;
