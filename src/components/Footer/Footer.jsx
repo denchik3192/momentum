bf8580517791fa36451d6c6a403c9556568cff37
DENIS <den465341@gmail.com>
2023-03-08 22:37:28 +0300

    fix" 420px resolution styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,29 @@
-
 import { Refresh } from "@mui/icons-material";
 import classNames from "classnames";
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchQuotes } from "../../reduxTK/quoteSlice";
 import s from "./footer.module.scss";
-import Settings from "./Settings/Settings";
-import ToDo from "./todo/ToDo";
+// import Settings from "./Settings/Settings";
+// import ToDo from "./todo/ToDo";
+
+const SettingsComponent = lazy(() => import("./Settings/Settings"));
+const ToDoComponent = lazy(() => import("./todo/ToDo"));
 
 const Footer = (props) => {
   const [todoActive, setTodoActive] = useState(false);
   const [settingsActive, setSettingsActive] = useState(false);
   const dispatch = useDispatch();
   const quoteData = useSelector((state) => state.quote);
-  const isQuoteSettingActive = useSelector((state) => state.settings.generalSettings[4].checked);
-  const isTodoSettingActive = useSelector((state) => state.settings.generalSettings[7].checked);
+  const isQuoteSettingActive = useSelector(
+    (state) => state.settings.generalSettings[4].checked
+  );
+  const isTodoSettingActive = useSelector(
+    (state) => state.settings.generalSettings[7].checked
+  );
   const toggleToDo = () => {
     todoActive ? setTodoActive(false) : setTodoActive(true);
   };
@@ -26,14 +32,14 @@ const Footer = (props) => {
     settingsActive ? setSettingsActive(false) : setSettingsActive(true);
   };
   useEffect(() => {
-      dispatch(fetchQuotes());
+    dispatch(fetchQuotes());
   }, [dispatch]);
 
   const refreshQuote = () => {
     dispatch(fetchQuotes());
   };
 
-  console.log('footer render');
+  console.log("footer render");
   return (
     <footer className={s.footer}>
       <Link
@@ -41,21 +47,32 @@ const Footer = (props) => {
         className={s.settingsButton}
         onClick={toggleSettings}
       ></Link>
-      <Settings active={settingsActive} setSettingsActive={setSettingsActive} />
-      <div className={classNames(s.qoute, isQuoteSettingActive ? '' : s.hidden)}>
-        <Refresh className={s.quoteButton}
-         onClick={refreshQuote}
-          />
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <SettingsComponent
+          active={settingsActive}
+          setSettingsActive={setSettingsActive}
+        />
+      </Suspense>
+
+      <div
+        className={classNames(s.qoute, isQuoteSettingActive ? "" : s.hidden)}
+      >
+        <Refresh className={s.quoteButton} onClick={refreshQuote} />
         {quoteData.status === "loading" ? (
           <div>{quoteData.status}</div>
         ) : (
           <div>"{quoteData.quote}"</div>
-          )}
+        )}
       </div>
-      <div className={classNames(s.todo, isTodoSettingActive ? '' : s.hidden )} onClick={toggleToDo}>
+      <div
+        className={classNames(s.todo, isTodoSettingActive ? "" : s.hidden)}
+        onClick={toggleToDo}
+      >
         ToDo
       </div>
-      <ToDo active={todoActive} onClick={toggleToDo} />
+      <Suspense>
+        <ToDoComponent active={todoActive} onClick={toggleToDo} />
+      </Suspense>
     </footer>
   );
 };
